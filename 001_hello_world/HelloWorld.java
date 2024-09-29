0df3af03dacc5121381f7e1e17572caba5b62c7d
ronnieqt <xi.ronnie.chen@outlook.com>
2024-09-29 19:37:11 +0100

    added hello world

---
+++ 001_hello_world/HelloWorld.java
@@ -0,0 +1,10 @@
+// A simple program the prints text on the terminal
+// File name must match the class declared in the file
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
